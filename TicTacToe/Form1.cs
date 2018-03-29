240c36d6b015703060bb99e3462b20a44e70cc6c
Lydia Fritz <Lydia.Fritz@gcu.edu>
2018-03-29 11:13:55 -0700

    created button class and added buttons to form

--- TicTacToe/Form1.cs
+++ TicTacToe/Form1.cs
@@ -12,9 +12,40 @@ namespace TicTacToe
 {
     public partial class Form1 : Form
     {
+        private TicTacToeButton[,] grid;
         public Form1( )
         {
             InitializeComponent( );
+
+            //test the button
+            TicTacToeButton tic_tac_toe_btn = new TicTacToeButton( );
+            
+            //set the grid
+            setGrid( );
+        }
+
+        private void setGrid( )
+        {
+            //place the buttons on the grid
+            Point loc = new Point( 5, 5 );
+            //make the array of buttons
+            grid = new TicTacToeButton[ 3, 3 ];
+            for(int r = 0; r < 3; r++)
+            {
+                for(int c = 0; c < 3; c++)
+                {
+                    //make a new button
+                    grid[ r, c ] = new TicTacToeButton( );
+                    this.Controls.Add( grid[ r, c ] );
+                    grid[ r, c ].Location = loc;
+                    //update location - go to next col
+                    loc.Y = loc.Y + grid[ r, c ].Width + 2;
+                }
+                //go to beginning of next row
+                loc.Y = 5;
+                loc.X = loc.X + grid[ r, 0 ].Height + 2;
+            }
+
         }
     }
 }
---
+++ TicTacToe/TicTacToeButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    class TicTacToeButton : Button
+    {
+        public TicTacToeButton( )
+        {
+            this.Width = this.Height = 60;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.Font = new System.Drawing.Font( "Consolas", 30.0F );
+            this.Text = "*";
+            this.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+
+        }
+    }
+}
