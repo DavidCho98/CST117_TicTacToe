b8633c30cdb065294d52c6d366219d8d9fa1f644
Lydia Fritz <lydiakfritz@gmail.com>
2018-04-03 13:16:11 -0700

    Add project files.

---
+++ TicTacToeV3/TicTacToeButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Forms;
+
+namespace TicTacToeV3
+{
+    class TicTacToeButton : Button
+    {
+        public bool Occupied = false;
+        public static int Btn_Size = 75;
+        
+
+        public TicTacToeButton( )
+        {
+            this.Width = this.Height = Btn_Size;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.Text = "*";
+            this.FlatStyle = FlatStyle.Flat;
+            this.Font = new System.Drawing.Font( "Consolas", 25 );
+        }
+    }
+}
